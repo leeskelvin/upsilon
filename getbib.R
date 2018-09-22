dcef38e3ffc8293ae9b2b27df4d0624db25bbf6f
leeskelvin <lee.s.kelvin@gmail.com>
2018-09-22 15:36:18 -0400

    updated getbib.R to work without astro package requirement.

--- getbib.R
+++ getbib.R
@@ -1,7 +1,7 @@
 #!/usr/bin/Rscript --no-init-file
 
 # definitions
-indir = normalizePath("~/work/papers")
+indir = normalizePath("~/Dropbox/work/papers")
 outrds = paste(indir, "/bibtex.rds", sep="")
 outbib = paste(indir, "/bibtex.bib", sep="")
 #outtemp = paste(indir, "/bibtex.temp.rds", sep="")
@@ -30,15 +30,76 @@ if(file.exists(outrds)){
     res[pos,] = old
 }
 
+# FUNCTION counter
+counter = function(i, total, text = "", overline = FALSE, underline = FALSE, endnewline = TRUE){
+    
+    toprint = paste("  ", i, " / ", total, " ", sep="")
+    if(text != ""){toprint = paste(toprint, ": ", text, " ", sep="")}
+    linelen = nchar(toprint) + 1
+    if(overline){
+        toprint = paste(paste(rep("-",linelen), collapse=""), "\n", toprint, sep="")
+        endnewline = FALSE
+    }
+    if(underline){
+        toprint = paste(toprint, "\n", paste(rep("-",linelen), collapse=""), sep="")
+        endnewline = FALSE
+    }
+    if(overline | underline){
+        toprint = paste(toprint, "\n", sep="")
+    }
+    if(!overline & !underline){
+        cat(paste(rep(c(" ","\b"),each=80),collapse=""))    # clear line
+        todelete = nchar(toprint)
+        backspace = paste(rep("\b", len=todelete), collapse="")
+        toprint = paste(toprint, backspace, sep="")
+    }
+    if(endnewline){
+        if(i == total){
+            toprint = paste(toprint, "\n", sep="")
+        }
+    }
+    cat(toprint)
+    
+}
+
+# FUNCTION strip
+strip = function(x, strip=" "){
+    
+    # workhorse function
+    sfunc = function(x, strip){
+        x = strsplit(x,"")[[1]]
+        for(i in 1:length(strip)){
+            s = strip[i]
+            good = which(x!=s)
+            if(length(good)>0){
+                x = x[min(good):max(good)]
+            }else{
+                x = ""
+            }
+        }
+        x = paste(x,collapse="")
+        return(x)
+    }
+    
+    # vectorize
+    stripped = as.character(Vectorize(sfunc, vectorize.args=c("x"))(x=x, strip=strip))
+    
+    # return results
+    return(stripped)
+    
+}
+
 i = 1
 
 # loop over each necessary entry
 lookup = which(res[,"year"]==-9999)
 for(i in lookup){
+    
     # setup
     ref = res[i,"ref"]
     code = res[i,"code"]
     counter(which(lookup==i), length(lookup), ref)
+    
 #    # check if a rescan of ADS is necessary
 #    rescan = TRUE
 #    if(ref %in% old[,"ref"]){
@@ -51,22 +112,28 @@ for(i in lookup){
 #            bibentry = old[row,"bibentry"][[1]]
 #        }
 #    }
+
 #    # rescan from ADS if necessary
 #    if(rescan){
     # rescan from ADS
     adsurl = paste(mirror, "/abs/", code, sep="")
     biburl = paste(mirror, "/cgi-bin/nph-bib_query?bibcode=", code, "&data_type=BIBTEX&db_key=AST&nocookieset=1", sep="")
+    
     # get BIBTEX
     bibfile = readLines(biburl)
+    
     # update reference
     firstline = grep("@",bibfile)[1]
     bibtype = tolower(strsplit(bibfile[firstline], "\\{")[[1]][1])
     bibfile[firstline] = paste(bibtype, "{", ref, ",", sep="")
+    
     # pick out metadata
     title = strip(strsplit(grep("title = ", bibfile, value=TRUE), "title =")[[1]][2], c(" ","{","}",",","\\",'"',"{","}"))
     year = as.numeric(strip(strsplit(grep("year =", bibfile, value=TRUE), "year =")[[1]][2], c(" ",","," ")))
+    
     # generate bibtext entry
     bibentry = bibfile[firstline:length(bibfile)]
+    
     # arXiv fix (MNRAS compatible)
     if(length(grep("e-prints", bibentry)) > 0){
         bibentry[grep("e-prints", bibentry)] = "  journal = {ArXiv e-prints},"
@@ -74,9 +141,11 @@ for(i in lookup){
         bibentry = bibentry[c(1:vrow,vrow:length(bibentry))]
         bibentry[vrow] = '   volume = "{\\!\\!}",'
     }
+    
     # replace %26 with ampersand (principally in HTML URLs)
     if(length(grep("%26", bibentry)) > 0){bibentry = gsub("%26", "&", bibentry)}
 #    }
+    
     # add to global
     #res[i,"ref"] = ref
     res[i,"year"] = year
@@ -85,8 +154,10 @@ for(i in lookup){
     #res[i,"adsurl"] = adsurl
     #res[i,"biburl"] = biburl
     res[i,"bibentry"][[1]] = list(bibentry)
+    
     ## save global (constant save in case of HTTP break)
     #saveRDS(res, file=outtemp)
+    
 }
 
 i = 1
