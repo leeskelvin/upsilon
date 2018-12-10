f8c479478dda948d2c016c7d98bf7c9896fc6091
Lee Kelvin <lee.s.kelvin@gmail.com>
2018-12-10 15:30:09 -0800

    updated setup files

--- setup.sh
+++ setup.sh
@@ -1,14 +1,25 @@
 #!/bin/sh
 
-ln -s upsilon/bashrc_cloud ~/.bashrc_cloud
-ln -s upsilon/Renviron ~/.Renviron
-ln -s upsilon/Rprofile ~/.Rprofile
+# new files/dirs
+touch ~/.bashrc_local
+mkdir -p ~/.R/lib
 
+# links
+ln -s bashrc_cloud ~/.bashrc_cloud
+ln -s Renviron ~/.Renviron
+ln -s Rprofile ~/.Rprofile
+
+# setup bashrc files
 echo "
 # source local bashrc file
 if [ -f ~/.bashrc_cloud ]; then
     . ~/.bashrc_cloud
 fi
 " >> ~/.bashrc
-
 source ~/.bashrc
+
+# refresh synaptic quick search
+sudo apt-get install synaptic
+sudo apt-get install apt-xapian-index
+sudo update-apt-xapian-index -vf
+
