e040f6bd6aadafd8aa2e29eba660cf4cb0e04779
Lee Kelvin <lee.s.kelvin@gmail.com>
2019-03-04 23:39:57 -0800

    palette update

--- setup/Rscript.R
+++ setup/Rscript.R
@@ -2,7 +2,7 @@
 
 # setup
 #require("astro", quietly=TRUE)
-#palette(c("#000000","#f1a340","#998ec3","#edf8b1","#7fcdbb","#2c7fb8"))
+#palette(c("#000000", "#e66101", "#5e3c99", "#fdb863", "#b2abd2", "#edf8b1", "#7fcdbb", "#2c7fb8"))
 set.seed(3125)
 
 # definitions
