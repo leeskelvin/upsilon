caed03c0883c3e7fa11ff3f397bd75ae5167be54
Lee Kelvin <lee.s.kelvin@gmail.com>
2019-10-04 14:42:42 -0400

    setup alias updates

--- setup/setup.sh
+++ setup/setup.sh
@@ -30,19 +30,22 @@ fi
 # setup bashrc_local
 if ! grep -q "aliases" ~/.bashrc_local; then
 echo "# aliases
-alias galfit='~/software/galfit/galfit'
-alias imfit='~/software/imfit/imfit'
-alias imfit-mcmc='~/software/imfit/imfit-mcmc'
-alias makeimage='~/software/imfit/makeimage'
-alias imarith='~/software/cexamples/imarith'
-alias listhead='~/software/cexamples/listhead'
-alias modhead='~/software/cexamples/modhead'
+#alias galfit='~/software/galfit/galfit'
+#alias imfit='~/software/imfit/imfit'
+#alias imfit-mcmc='~/software/imfit/imfit-mcmc'
+#alias makeimage='~/software/imfit/makeimage'
+#alias imarith='~/software/cexamples/imarith'
+#alias listhead='~/software/cexamples/listhead'
+#alias modhead='~/software/cexamples/modhead'
 
 # ssh
 alias atlas='ssh -X lee@atlas.st-andrews.ac.uk'
 alias external='ssh -t astlkelv@external.astro.ljmu.ac.uk \"bash\"'
-alias external2='ssh -t astlkelv@external2.astro.ljmu.ac.uk \"bash\"'
 alias xsinister='ssh -t astlkelv@external.astro.ljmu.ac.uk \"ssh -t astlkelv@sinister.astro.ljmu.ac.uk bash\"'
+alias physauth='ssh lskelvin@physauth.physics.ucdavis.edu'
+alias xemerald='ssh -t lskelvin@physauth.physics.ucdavis.edu \"ssh -t lskelvin@emerald.physics.ucdavis.edu bash\"'
+alias minos='ssh -X lkelvin@minos.astro.princeton.edu'
+alias lsstdev='ssh -X lskelvin@lsst-dev01.ncsa.illinois.edu'
 " >> ~/.bashrc_local
 fi
 
