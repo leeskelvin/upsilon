f77285a5330730c6e782296b57860266f75657b2
Lee Kelvin <lkelvin@astro.princeton.edu>
2020-01-27 15:18:15 -0600

    ipython/ds9 bashcloud update

--- setup/ipython-startup.py
+++ setup/ipython-startup.py
@@ -1,6 +1,17 @@
+# system imports
+import sys
+import os
 import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
 matplotlib.use('Qt5Agg')
 plt.ion()
 
+# LSK defaults
+
+
+# LSST stack imports
+import lsst.daf.persistence as dafPersist
+import lsst.afw.display as afwDisplay
+from lsst.ip.isr import IsrTask
+import lsst.afw.detection as afwDetection
