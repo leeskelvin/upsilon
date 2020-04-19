677edf4ac4347304b9f4b26e796f471e93be7824
Lee Kelvin <lkelvin@astro.princeton.edu>
2020-04-19 13:58:25 -0500

    ipython updates

--- setup/ipython-startup.py
+++ setup/ipython-startup.py
@@ -10,15 +10,16 @@ from collections import Counter as count
 import pandas as pd
 import pickle
 
-# plotting specific
-matplotlib.use('Qt5Agg')
-plt.ion()
+## plotting specific
+#matplotlib.use('Qt5Agg')
+#plt.ion()
 
-# LSST stack imports
-try:
-    import lsst.daf.persistence as dafPersist
-    import lsst.afw.display as afwDisplay
-    from lsst.ip.isr import IsrTask
-    import lsst.afw.detection as afwDetection
-except ImportError:
-    warnings.warn('failed to import LSST modules')
+## LSST stack imports
+#try:
+#    import lsst.daf.persistence as dafPersist
+#    import lsst.afw.display as afwDisplay
+#    from lsst.ip.isr import IsrTask
+#    import lsst.afw.detection as afwDetection
+#except ImportError:
+#    warnings.warn('failed to import LSST modules')
+
